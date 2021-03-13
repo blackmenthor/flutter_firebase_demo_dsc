8f09a3260fab59f1123f8c2bb2c8da13ddd52a56
Angga Dwi Arifandi <namapanjanggua@gmail.com>
2021-03-13 12:17:45 +0700

    impl.

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,23 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,84 +28,147 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+const SHOW_BTN_KEY = 'show_button';
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  RemoteConfig _remoteConfig;
+  Timer _timer;
+
+  var _showBtn = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      setupRemoteConfig();
+    });
+  }
+
+  void setupRemoteConfig() async {
+    _remoteConfig = RemoteConfig.instance;
+    // Allow a fetch every millisecond. Default is 12 hours.
+    _remoteConfig.setConfigSettings(
+      RemoteConfigSettings(
+        minimumFetchInterval: Duration(seconds: 1),
+        fetchTimeout: Duration(minutes: 5),
+      ),
+    );
+    _remoteConfig.setDefaults(<String, dynamic>{
+      SHOW_BTN_KEY: 1,
+    });
+
+    await _remoteConfig.fetch();
+    await _remoteConfig.fetchAndActivate();
+    _showBtn = (_remoteConfig?.getInt(SHOW_BTN_KEY) ?? 1) == 1;
+
+    _timer = Timer.periodic(Duration(seconds: 3), (timer) async {
+      await _remoteConfig.fetch();
+      await _remoteConfig.fetchAndActivate();
+      _showBtn = (_remoteConfig?.getInt(SHOW_BTN_KEY) ?? 1) == 1;
+
+      setState(() {});
+    });
+
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _timer?.cancel();
+    _remoteConfig?.dispose();
+  }
+
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    Query query = FirebaseFirestore.instance.collection('users');
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: StreamBuilder<QuerySnapshot>(
+        stream: query.snapshots(),
+        builder: (context, stream) {
+          if (stream.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (stream.hasError) {
+            return Center(child: Text(stream.error.toString()));
+          }
+
+          QuerySnapshot querySnapshot = stream.data;
+
+          return ListView.builder(
+            itemCount: querySnapshot.size,
+            itemBuilder: (context, index) => UserWidget(
+              name: querySnapshot.docs[index]['name'],
+              phone: querySnapshot.docs[index]['phone'],
+              key: ValueKey(querySnapshot.docs[index].id),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: _showBtn
+          ? FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: Icon(Icons.add),
+            )
+          : Container(),
+    );
+  }
+}
+
+class UserWidget extends StatelessWidget {
+  final String name;
+  final String phone;
+
+  UserWidget({
+    @required this.name,
+    @required this.phone,
+    ValueKey key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            name,
+            style: TextStyle(
+              fontSize: 18.0,
+            ),
+          ),
+          Text(
+            phone,
+            style: TextStyle(
+              fontSize: 16.0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
